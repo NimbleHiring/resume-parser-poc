99b4c8f155e5d7968219ccf739249e17f8f552f2
Dillon Brock <dillon@hirenimble.com>
2025-04-02 09:56:46 -0400

    Add LLM provider classes

---
+++ src/providers.ts
@@ -0,0 +1,73 @@
+import OpenAI, { ClientOptions } from "openai";
+
+type LLMResponse = OpenAI.Responses.Response;
+
+interface LLMClient {
+    sendMessage(message: string): Promise<LLMResponse>;
+    validateResponse(response: LLMResponse): boolean;
+    getResponseText(response: LLMResponse): string;
+}
+
+
+class OpenAIClient implements LLMClient {
+  private client: OpenAI;
+  private model: string;
+
+  constructor(config: ClientOptions, model: string) {
+    this.client = new OpenAI(config);
+    this.model = model;
+  }
+
+  async sendMessage(message: string) {
+    return this.client.responses.create({
+      model: this.model,
+      instructions: 'instructions',
+      input: message,
+    });
+  }
+
+  validateResponse(response: LLMResponse): boolean {
+    if (!response.error) return true;
+    console.error(response.error);
+    return false;
+  }
+
+  getResponseText(response: OpenAI.Responses.Response): string {
+    return response.output_text;
+  }
+}
+
+export class LLMProvider {
+  private client: LLMClient | null = null;
+  private provider: string = process.env.LLM_PROVIDER || "openai";
+
+  private createClient(): LLMClient {
+    switch (this.provider) {
+      case "openai":
+        return new OpenAIClient({
+          apiKey: process.env.OPENAI_API_KEY,
+        }, 'gpt-4o');
+      default:
+        throw new Error(`Unsupported LLM provider: ${this.provider}`);
+    }
+  }
+
+  private getClient(): LLMClient {
+    if (!this.client) {
+      const client = this.createClient();
+      this.client = client;
+    }
+
+    return this.client;
+  }
+
+  async sendMessage(message: string): Promise<LLMResponse> {
+    const client = this.getClient();
+    return client.sendMessage(message);
+  }
+
+  getResponseText(response: LLMResponse): string {
+    const client = this.getClient();
+    return client.getResponseText(response);
+  }
+}
